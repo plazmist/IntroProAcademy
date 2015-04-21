5f13a6c182e625221d5da81ecdac57b64ffba2ed
dkomisar <dkomisar@intropro.com>
2015-04-21 09:37:58 +0300

    first task

---
+++ src/com/dkomisar/Main.java
@@ -0,0 +1,20 @@
+package com.dkomisar;
+
+import com.dkomisar.task1.Anagramm;
+
+public class Main {
+
+    public static void callAnagramm(String input){
+        Anagramm anag1 = new Anagramm();
+        anag1.setMyCharArray(input);
+        System.out.println("What we have    : " + anag1.getMyCharArray());
+        anag1.anagrammString();
+        System.out.println("What we have got: " + anag1.getMyCharArray() + "\n");
+
+    }
+
+    public static void main(String[] args) {
+        callAnagramm("a ab abc abcd abcde 1 12 123 1234 12345   12345     123 gfgkaade 111111");
+        callAnagramm("12.3 a.bcd   a1b2c3d4");
+    }
+}
